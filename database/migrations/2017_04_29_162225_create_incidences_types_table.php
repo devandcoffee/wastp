ec89d03d851075225901551fa1458f9a42f76fc4
javieroc <ferroxido@gmail.com>
2017-04-30 00:21:20 -0300

    Adding migrations

--- database/migrations/2017_04_29_162225_create_incidences_types_table.php
+++ database/migrations/2017_04_29_162225_create_incidences_types_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTeamsTable extends Migration
+class CreateIncidencesTypesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class CreateTeamsTable extends Migration
      */
     public function up()
     {
-        Schema::create('teams', function (Blueprint $table) {
+        Schema::create('incidences_types', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->integer('amount_players');
-            $table->string('logo_url');
-            $table->integer('user_id');
             $table->timestamps();
         });
     }
@@ -30,6 +27,6 @@ class CreateTeamsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('teams');
+        Schema::dropIfExists('incidences_types');
     }
 }
