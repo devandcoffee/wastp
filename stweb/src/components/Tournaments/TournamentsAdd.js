464f0058eda27b2b05b1b4822fb0ebf428a5511c
Facundo Flores <flores.facundogabriel@gmail.com>
2017-06-17 11:26:52 -0300

    Add form for adding tournaments

--- stweb/src/components/Tournaments/TournamentsAdd.js
+++ stweb/src/components/Tournaments/TournamentsAdd.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
-import { Button } from 'antd';
+import { Button, Card, Row, Form, Input, DatePicker, InputNumber } from 'antd';
+
+const FormItem = Form.Item;
 
 class TournamentsAdd extends Component {
-
   render() {
     return (
       <div>
-        <Button onClick={this.props.backToList}> Back to List </Button>
+        <Row>
+          <Button onClick={this.props.backToList}> Back to List </Button>
+        </Row>
+        <Row>
+          <Card title="New Tournament" bordered={false} style={{ width: '100%', marginTop: '25px' }}>
+            <Form onSubmit={this.handleSubmit}>
+              <FormItem label="Name">
+                <Input placeholder="Name" />
+              </FormItem>
+              <FormItem label="Start Date">
+                <DatePicker format="DD-MM-YYYY HH:mm:ss" showTime />
+              </FormItem>
+              <FormItem label="Amount Teams">
+                <InputNumber min={2} max={50} />
+              </FormItem>
+              <FormItem label="Description">
+                <Input type="textarea" rows={4} />
+              </FormItem>
+              <FormItem>
+                <Button type="primary" htmlType="submit" size="large" icon="save">Save</Button>
+              </FormItem>
+            </Form>
+          </Card>
+        </Row>
       </div>
     )
   }
