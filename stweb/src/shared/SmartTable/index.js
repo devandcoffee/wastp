337c156d3baac909eb2b52f1a9785258e5214b9d
Facundo Flores <flores.facundogabriel@gmail.com>
2017-06-24 07:38:45 -0300

    Add smarttable component

---
+++ stweb/src/shared/SmartTable/index.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { PropTypes } from "prop-types";
+import { Button, Table } from 'antd';
+import * as TABLE_ACTIONS from './constants';
+
+const ACTIONS_OBJ = (emit) => ({
+  title: 'Action',
+  dataIndex: '',
+  key: 'x',
+  render: (text, record, index) => (
+    <span>
+      <Button icon="search" onClick={() => emit(text, record, index, TABLE_ACTIONS.SHOW_RECORD)} />
+      <Button icon="edit" onClick={() => emit(text, record, index, TABLE_ACTIONS.EDIT_RECORD)} />
+      <Button icon="delete" onClick={() => emit(text, record, index, TABLE_ACTIONS.REMOVE_RECORD)} />
+    </span>
+  )
+})
+
+class SmartTable extends Component {
+  constructor(props) {
+    super(props);
+    const { columns, dataSource, emit, rowKey } = this.props;
+    this.state = {
+      columns: columns.concat(ACTIONS_OBJ(emit)),
+      dataSource,
+      rowKey
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({ dataSource: nextProps.dataSource })
+  }
+
+  render() {
+    const { columns, dataSource, rowKey } = this.state;
+    return (
+      <Table dataSource={dataSource} columns={columns} rowKey={rowKey} />
+    );
+  }
+}
+
+SmartTable.defaultProps = {
+  rowKey: ''
+}
+
+SmartTable.propTypes = {
+  dataSource: PropTypes.array.isRequired,
+  emit: PropTypes.func.isRequired,
+  rowKey: PropTypes.string,
+}
+
+export default SmartTable;
