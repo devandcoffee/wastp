402aace508cfff5c8cf56ac7cdbc2bde8bae2468
Matias Novoa <mattgaviota@gmail.com>
2017-08-02 16:42:40 -0300

    Cambios en la configuración del deploy

--- stapi/deploy.php
+++ stapi/deploy.php
@@ -4,59 +4,99 @@ namespace Deployer;
 require 'vendor/deployer/recipes/local.php';
 require 'recipe/laravel.php';
 
-// Configuration
+// Settings
+
+// ssh settings
 set('ssh_type', 'native');
 set('ssh_multiplexing', true);
 
+// local paths
 set('local_release_path', '/tmp/wastp');
+set('local_stapi', '{{local_release_path}}/stapi');
+set('local_stweb', '{{local_release_path}}/stweb');
 
+// deploy paths
+set('deploy_path', '/home/wastp/deploy');
+set('deploy_wastp', '{{deploy_path}}/current/wastp');
+set('deploy_stapi', '{{deploy_path}}/current/wastp/stapi');
+set('deploy_stweb', '{{deploy_path}}/current/wastp/stweb');
+
+set('local_releases_list', function () {
+    $list = null;
+    return $list;
+});
+
+// Repository settings
 set('repository', 'git@github.com:devandcoffee/wastp.git');
-
 set('branch', 'master');
-
-add('shared_files', []);
-add('shared_dirs', []);
-add('writable_dirs', []);
 
 // Hosts
 host('104.236.121.143')
     ->stage('testing')
     ->user('wastp')
     ->forwardAgent(true)
-    ->set('deploy_path', '/home/wastp/deploy');
+    ->set('deploy_path', '{{deploy_path}}');
 
 // Tasks
+desc('Upload local folder on the remote server');
 task('upload_code', function() {
     upload(get('local_release_path'), get('release_path'));
 });
 
+desc('Remove relase temporal folder');
 task('remove_local_release', function() {
     runLocally("rm -rf {{local_release_path}}");
 });
 
+desc('Load docker containers');
 task('docker_compose_up', function() {
-    run("cd {{deploy_path}} && docker-compose up -d");
+    run("cd {{deploy_wastp}} && docker-compose up -d");
 });
 
+desc('Install composer packages on stapi');
 task('composer_install', function() {
-    runLocally("cd {{local_release_path}}/stapi && composer install --no-dev");
+    runLocally("cd {{local_stapi}} && {{bin/composer}} {{composer_options}}");
 });
 
-// [Optional] if deploy fails automatically unlock.
+desc('Install npm packages on stweb');
+task('npm_install', function() {
+    runLocally("cd {{local_stweb}} && npm install");
+});
+
+// Artisan tasks
+desc('Execute artisan migrate');
+task('artisan_migrate', function() {
+    run('{{bin/php}} {{deploy_stapi}}/artisan migrate --force');
+});
+
+desc('Execute artisan optimize');
+task('artisan_optimize', function () {
+    run('{{bin/php}} {{deploy_stapi}}/artisan optimize');
+});
+
+desc('Execute artisan cache:clear');
+task('artisan_cache:clear', function () {
+    run('{{bin/php}} {{deploy_stapi}}/artisan cache:clear');
+});
+
+// [Optional] if deploy fails automatically unlock and remove local folder.
 after('deploy:failed', 'deploy:unlock');
+after('deploy:failed', 'remove_local_release');
 
 // Migrate database before symlink new release.
+before('deploy:symlink', 'artisan_migrate');
 
-before('deploy:symlink', 'artisan:migrate');
-
+// Main deploy task
 task('deploy', [
     'deploy:prepare',
     'deploy:lock',
     'deploy:release',
     'local:update_code',
     'composer_install',
-    'artisan:cache:clear',
-    'artisan:optimize',
+    'npm_install',
+    'upload_code',
+    'artisan_cache:clear',
+    'artisan_optimize',
     'docker_compose_up',
     'deploy:symlink',
     'deploy:unlock',
